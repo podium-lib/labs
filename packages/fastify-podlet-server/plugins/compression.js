cd2b38520e3b8a7ad039f1fdc0de6de141b2e1c4
Richard Walker <digitalsadhu@gmail.com>
2023-03-10 14:40:25 +1300

    add test for script plugin

--- packages/fastify-podlet-server/plugins/compression.js
+++ packages/fastify-podlet-server/plugins/compression.js
@@ -2,16 +2,7 @@ import compress from "@fastify/compress";
 import fp from "fastify-plugin";
 
 export default fp(async function compression(fastify, { enabled }) {
-    if (enabled) {
-        await fastify.register(compress, { global: true });
-
-        // wrap the markup in a podlet render call
-        // fastify.addHook("onSend", (request, reply, /** @type {string} */ payload) => {
-        //     // @ts-ignore
-        //     // reply.compress(payload);
-        //     reply.send("Nah ahhh")
-        //     // done();
-        //     return reply;
-        // });
-      }
+  if (enabled) {
+    await fastify.register(compress, { global: true });
+  }
 });
